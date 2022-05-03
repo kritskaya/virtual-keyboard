228747f13d00969cb868fd494de5d5e0ce3dde8b
Tatyana Kritskaya <kritskaya.tatjana@gmail.com>
2022-05-03 21:20:37 +0300

    feat: add property to keys array data

--- src/js/keys.js
+++ src/js/keys.js
@@ -5,98 +5,112 @@ const keys = [
 		eng: "`",
 		engshift: "~",
 		rus: "ё",
-		russhift: "Ё"
+		russhift: "Ё",
+		row: 1
 	},
 	{
 		name: "key-1",
 		eng: "1",
 		engshift: "!",
 		rus: "1",
-		russhift: "!"
+		russhift: "!",
+		row: 1
 	},
 	{
 		name: "key-2",
 		eng: "2",
 		engshift: "@",
 		rus: "2",
-		russhift: "\""
+		russhift: "\"",
+		row: 1
 	},
 	{
 		name: "key-3",
 		eng: "3",
 		engshift: "#",
 		rus: "3",
-		russhift: "№"
+		russhift: "№",
+		row: 1
 	},
 	{
 		name: "key-4",
 		eng: "4",
 		engshift: "$",
 		rus: "4",
-		russhift: ";"
+		russhift: ";",
+		row: 1
 	},
 	{
 		name: "key-5",
 		eng: "5",
 		engshift: "%",
 		rus: "5",
-		russhift: "%"
+		russhift: "%",
+		row: 1
 	},
 	{
 		name: "key-6",
 		eng: "6",
 		engshift: "^",
 		rus: "6",
-		russhift: ":"
+		russhift: ":",
+		row: 1
 	},
 	{
 		name: "key-7",
 		eng: "7",
 		engshift: "&",
 		rus: "7",
-		russhift: "?"
+		russhift: "?",
+		row: 1
 	},
 	{
 		name: "key-8",
 		eng: "8",
 		engshift: "*",
 		rus: "8",
-		russhift: "*"
+		russhift: "*",
+		row: 1
 	},
 	{
 		name: "key-9",
 		eng: "9",
 		engshift: "(",
 		rus: "9",
-		russhift: "("
+		russhift: "(",
+		row: 1
 	},
 	{
 		name: "key-0",
 		eng: "0",
 		engshift: ")",
 		rus: "0",
-		russhift: ")"
+		russhift: ")",
+		row: 1
 	},
 	{
 		name: "key-minus",
 		eng: "-",
 		engshift: "_",
 		rus: "-",
-		russhift: "_"
+		russhift: "_",
+		row: 1
 	},
 	{
 		name: "key-equal",
 		eng: "=",
 		engshift: "+",
 		rus: "=",
-		russhift: "+"
+		russhift: "+",
+		row: 1
 	},
 	{
 		name: "key-func-backspace",
 		eng: "",
 		engshift: "",
 		rus: "",
-		russhift: ""
+		russhift: "",
+		row: 1
 	},
 
 	// row 2
@@ -105,105 +119,120 @@ const keys = [
 		eng: "",
 		engshift: "",
 		rus: "",
-		russhift: ""
+		russhift: "",
+		row: 2
 	},
 	{
 		name: "key-q",
 		eng: "q",
 		engshift: "Q",
 		rus: "й",
-		russhift: "Й"
+		russhift: "Й",
+		row: 2
 	},
 	{
 		name: "key-w",
 		eng: "w",
 		engshift: "W",
 		rus: "ц",
-		russhift: "Ц"
+		russhift: "Ц",
+		row: 2
 	},
 	{
 		name: "key-e",
 		eng: "e",
 		engshift: "E",
 		rus: "у",
-		russhift: "У"
+		russhift: "У",
+		row: 2
 	},
 	{
 		name: "key-r",
 		eng: "r",
 		engshift: "R",
 		rus: "к",
-		russhift: "К"
+		russhift: "К",
+		row: 2
 	},
 	{
 		name: "key-t",
 		eng: "t",
 		engshift: "T",
 		rus: "е",
-		russhift: "Е"
+		russhift: "Е",
+		row: 2
 	},
 	{
 		name: "key-y",
 		eng: "y",
 		engshift: "Y",
 		rus: "н",
-		russhift: "Н"
+		russhift: "Н",
+		row: 2
 	},
 	{
 		name: "key-u",
 		eng: "u",
 		engshift: "U",
 		rus: "г",
-		russhift: "Г"
+		russhift: "Г",
+		row: 2
 	},
 	{
 		name: "key-i",
 		eng: "i",
 		engshift: "I",
 		rus: "ш",
-		russhift: "Ш"
+		russhift: "Ш",
+		row: 2
 	},
 	{
 		name: "key-o",
 		eng: "o",
 		engshift: "O",
 		rus: "щ",
-		russhift: "Щ"
+		russhift: "Щ",
+		row: 2
 	},
 	{
 		name: "key-p",
 		eng: "p",
 		engshift: "P",
 		rus: "з",
-		russhift: "З"
+		russhift: "З",
+		row: 2
 	},
 	{
 		name: "key-left-bracket",
 		eng: "[",
 		engshift: "{",
 		rus: "х",
-		russhift: "Х"
+		russhift: "Х",
+		row: 2
 	},
 	{
 		name: "key-right-bracket",
 		eng: "]",
 		engshift: "}",
 		rus: "ъ",
-		russhift: "Ъ"
+		russhift: "Ъ",
+		row: 2
 	},
 	{
 		name: "key-backslash",
 		eng: "\\",
 		engshift: "|",
 		rus: "\\",
-		russhift: "/"
+		russhift: "/",
+		row: 2
 	},
 	{
 		name: "key-func-del",
 		eng: "",
 		engshift: "",
 		rus: "",
-		russhift: ""
+		russhift: "",
+		row: 2
 	},
 
 	// row 3
@@ -212,91 +241,104 @@ const keys = [
 		eng: "",
 		engshift: "",
 		rus: "",
-		russhift: ""
+		russhift: "",
+		row: 3
 	},
 	{
 		name: "key-a",
 		eng: "a",
 		engshift: "A",
 		rus: "ф",
-		russhift: "Ф"
+		russhift: "Ф",
+		row: 3
 	},
 	{
 		name: "key-s",
 		eng: "s",
 		engshift: "S",
 		rus: "ы",
-		russhift: "Ы"
+		russhift: "Ы",
+		row: 3
 	},
 	{
 		name: "key-d",
 		eng: "d",
 		engshift: "D",
 		rus: "в",
-		russhift: "В"
+		russhift: "В",
+		row: 3
 	},
 	{
 		name: "key-f",
 		eng: "f",
 		engshift: "F",
 		rus: "а",
-		russhift: "А"
+		russhift: "А",
+		row: 3
 	},
 	{
 		name: "key-g",
 		eng: "g",
 		engshift: "G",
 		rus: "п",
-		russhift: "П"
+		russhift: "П",
+		row: 3
 	},
 	{
 		name: "key-h",
 		eng: "h",
 		engshift: "H",
 		rus: "р",
-		russhift: "Р"
+		russhift: "Р",
+		row: 3
 	},
 	{
 		name: "key-j",
 		eng: "j",
 		engshift: "J",
 		rus: "о",
-		russhift: "О"
+		russhift: "О",
+		row: 3
 	},
 	{
 		name: "key-k",
 		eng: "k",
 		engshift: "K",
 		rus: "л",
-		russhift: "Л"
+		russhift: "Л",
+		row: 3
 	},
 	{
 		name: "key-l",
 		eng: "l",
 		engshift: "L",
 		rus: "д",
-		russhift: "Д"
+		russhift: "Д",
+		row: 3
 	},
 	{
 		name: "key-semicolon",
 		eng: ";",
 		engshift: ":",
 		rus: "ж",
-		russhift: "Ж"
+		russhift: "Ж",
+		row: 3
 	},
 	{
 		name: "key-quote",
 		eng: "'",
 		engshift: "\"",
 		rus: "э",
-		russhift: "Э"
+		russhift: "Э",
+		row: 3
 	},
 	{
 		name: "key-func-enter",
 		eng: "",
 		engshift: "",
 		rus: "",
-		russhift: ""
+		russhift: "",
+		row: 3
 	},
 
 	// row 4
@@ -305,91 +347,104 @@ const keys = [
 		eng: "",
 		engshift: "",
 		rus: "",
-		russhift: ""
+		russhift: "",
+		row: 4
 	},
 	{
 		name: "key-z",
 		eng: "z",
 		engshift: "Z",
 		rus: "я",
-		russhift: "Я"
+		russhift: "Я",
+		row: 4
 	},
 	{
 		name: "key-x",
 		eng: "x",
 		engshift: "X",
 		rus: "ч",
-		russhift: "Ч"
+		russhift: "Ч",
+		row: 4
 	},
 	{
 		name: "key-c",
 		eng: "c",
 		engshift: "C",
 		rus: "с",
-		russhift: "С"
+		russhift: "С",
+		row: 4
 	},
 	{
 		name: "key-v",
 		eng: "v",
 		engshift: "V",
 		rus: "м",
-		russhift: "М"
+		russhift: "М",
+		row: 4
 	},
 	{
 		name: "key-b",
 		eng: "b",
 		engshift: "B",
 		rus: "и",
-		russhift: "И"
+		russhift: "И",
+		row: 4
 	},
 	{
 		name: "key-n",
 		eng: "n",
 		engshift: "N",
 		rus: "т",
-		russhift: "Т"
+		russhift: "Т",
+		row: 4
 	},
 	{
 		name: "key-m",
 		eng: "m",
 		engshift: "M",
 		rus: "ь",
-		russhift: "Ь"
+		russhift: "Ь",
+		row: 4
 	},
 	{
 		name: "key-comma",
 		eng: ",",
 		engshift: "&lt;",
 		rus: "б",
-		russhift: "Б"
+		russhift: "Б",
+		row: 4
 	},
 	{
 		name: "key-dot",
 		eng: ".",
 		engshift: "&gt;",
 		rus: "ю",
-		russhift: "Ю"
+		russhift: "Ю",
+		row: 4
 	},
 	{
 		name: "key-slash",
 		eng: "/",
 		engshift: "?",
 		rus: ".",
-		russhift: ","
+		russhift: ",",
+		row: 4
 	},
 	{
 		name: "key-func-up",
 		eng: "",
 		engshift: "",
 		rus: "",
-		russhift: ""
+		russhift: "",
+		row: 4
 	},
 	{
 		name: "key-func-right-shift",
 		eng: "",
 		engshift: "",
 		rus: "",
-		russhift: ""
+		russhift: "",
+		row: 4
 	},
 
 	// row 5
@@ -398,62 +453,71 @@ const keys = [
 		eng: "",
 		engshift: "",
 		rus: "",
-		russhift: ""
+		russhift: "",
+		row: 5
 	},
 	{
 		name: "key-func-win",
 		eng: "",
 		engshift: "",
 		rus: "",
-		russhift: ""
+		russhift: "",
+		row: 5
 	},
 	{
 		name: "key-func-left-alt",
 		eng: "",
 		engshift: "",
 		rus: "",
-		russhift: ""
+		russhift: "",
+		row: 5
 	},
 	{
 		name: "key-func-space",
 		eng: "",
 		engshift: "",
 		rus: "",
-		russhift: ""
+		russhift: "",
+		row: 5
 	},
 	{
 		name: "key-func-right-alt",
 		eng: "",
 		engshift: "",
 		rus: "",
-		russhift: ""
+		russhift: "",
+		row: 5
 	},
 	{
 		name: "key-func-left",
 		eng: "",
 		engshift: "",
 		rus: "",
-		russhift: ""
+		russhift: "",
+		row: 5
 	},
 	{
 		name: "key-func-down",
 		eng: "",
 		engshift: "",
 		rus: "",
-		russhift: ""
+		russhift: "",
+		row: 5
 	},
 	{
 		name: "key-func-right",
 		eng: "",
 		engshift: "",
 		rus: "",
-		russhift: ""
+		russhift: "",
+		row: 5
 	},
 	{
 		name: "key-func-right-ctrl",
 		eng: "",
 		engshift: "",
 		rus: "",
-		russhift: ""
+		russhift: "",
+		row: 5
 	},
 ]
